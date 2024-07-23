56f83226dd6568fde737bb35adeea3d878143377
KHY90 <power4206@gmail.com>
2024-07-23 14:26:54 +0900

    flux:front

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+/* 추가 스타일이 필요하다면 여기에 작성 */
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">FLUX</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Market</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Article</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.navbar {
+  padding-top: 1em !important;
+  padding-bottom: 1em !important;
+}
+
+.bg-body-tertiary {
+  background-color: #000 !important;
+}
+
+.navbar-brand {
+  font-family: 'PuradakGentleGothicR';
+  color: #FEBE98;
+  font-size: 36px;
+  margin-left: 20px;
+}
+
+.nav-link {
+  color: #fff !important;
+  font-family: 'LINESeedKR-Bd';
+  font-size: 21px;
+  margin-left: 10px;
+}
+
+.navbar-toggler-icon {
+  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba%28255, 255, 255, 1%29' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+}
+</style>
