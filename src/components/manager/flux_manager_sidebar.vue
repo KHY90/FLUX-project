caeaa7d3a77260f4c748cf001f33d954cc85429d
KHY90 <power4206@gmail.com>
2024-07-25 10:56:07 +0900

    feat:manager

---
+++ src/components/manager/flux_manager_sidebar.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="wrapper">
+      <div class="border-right sidebar-con d-flex flex-column" id="sidebar-wrapper">
+        <div class="sidebar-heading sidebar-title">DashBoard</div>
+        <div class="list-group list-group-flush">
+          <div class="sidebar-heading">
+            <a href="#" class="list-group-item list-group-item-action pl-4 sidebar-text">홈화면</a>
+            <hr>
+            <a href="#" class="list-group-item list-group-item-action pl-4 sidebar-text">회원목록</a>
+            <a href="#" class="list-group-item list-group-item-action pl-4 sidebar-text">회원권한설정</a>
+            <hr>
+            <a href="#" class="list-group-item list-group-item-action pl-4 sidebar-text">아티클목록</a>
+            <a href="#" class="list-group-item list-group-item-action pl-4 sidebar-text">아티클등록</a>
+            <hr>
+            <a href="#" class="list-group-item list-group-item-action pl-4 sidebar-text">공지목록</a>
+            <a href="#" class="list-group-item list-group-item-action pl-4 sidebar-text">공지등록</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ManagerSide',
+  }
+  </script>
+  
+  <style scoped>
+  .wrapper {
+    background-color: #000 !important;
+    border-top-right-radius: 10px; /* 좌상단 곡률 */
+    border-bottom-right-radius: 10px; /* 좌하단 곡률 */
+  }
+  
+  #sidebar-wrapper {
+    width: 250px;
+  }
+  
+  .sidebar-con {
+    width: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  
+  .sidebar-heading {
+    font-size: 1.5rem;
+    padding: 1rem;
+    color: white;
+  }
+  
+  .sidebar-img {
+    width: auto;
+    height: 50px;
+  }
+  
+  .sidebar-title {
+    font-family: "LINESeedKR-Bd";
+    font-size: 28px;
+    color: #1244AF;
+    margin-top: 30px;
+  }
+  
+  .sidebar-subtitle {
+    font-family: "LINESeedKR-Bd";
+    font-size: 21px;
+    color: #FFFFFF;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  
+  .sidebar-text {
+    font-family: "LINESeedKR-Bd";
+    font-size: 16px;
+    background-color: #000 !important;
+    color: #FFFFFF;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    text-align: center;
+  }
+  .title-link{
+    font-family: "LINESeedKR-Bd";
+    font-size: 12px;
+    color: #FFFFFF;
+  }
+  .title-link2{
+    font-family: "LINESeedKR-Bd";
+    font-size: 10px;
+    color: #FFFFFF;
+  }
+  
+  .list-group-item {
+    border: none !important;
+  }
+  
+  @media (max-width: 992px) {
+    #sidebar-wrapper {
+      margin-left: 0;
+    }
+    .wrapper{
+        display: none !important;
+    }
+  }
+  </style>
+  
